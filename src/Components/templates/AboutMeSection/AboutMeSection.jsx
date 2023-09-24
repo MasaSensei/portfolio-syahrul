0a49e3a022a58b9d202bf564a8be088bb88dedff
Masayoshi <masasensei26@mail.com>
2023-09-24 12:33:12 +0700

    done with image

--- src/Components/templates/AboutMeSection/AboutMeSection.jsx
+++ src/Components/templates/AboutMeSection/AboutMeSection.jsx
@@ -36,7 +36,7 @@ const AboutMeSection = () => {
             </div>
             <div className="flex-1 lg:order-2">
               <img
-                src="/assets/images/about_me.png"
+                src="/assets/images/about_me.jpeg"
                 alt=""
                 className="w-full h-full rounded-lg"
               />
